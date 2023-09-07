28123472db287ff49c0512a9aea142faa0355c55
ciaranjkm <ciaranjkm@gmail.com>
2023-09-07 04:27:52 +0100

    Optimise copyToClipboard() function.

--- password-generator.py
+++ password-generator.py
@@ -26,8 +26,7 @@ def genPassword():
     passString.set(tPassword)
     
 def copyToClipboard():
-    toCopy = passString.get()
-    pyperclip.copy(toCopy)
+    pyperclip.copy(passString.get())
 
 lengths = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24]
 
